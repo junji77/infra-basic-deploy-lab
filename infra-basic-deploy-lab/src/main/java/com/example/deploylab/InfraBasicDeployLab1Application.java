64083d530bd2a3a660eae0d2c51fe077aa4b08e8
junji77 <junjihun17@gmail.com>
2026-03-23 11:05:34 +0900

    수정 내용

--- infra-basic-deploy-lab/src/main/java/com/example/deploylab/InfraBasicDeployLab1Application.java
+++ infra-basic-deploy-lab/src/main/java/com/example/deploylab/InfraBasicDeployLab1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InfraBasicDeployLabApplication {
+public class InfraBasicDeployLab1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InfraBasicDeployLabApplication.class, args);
+		SpringApplication.run(InfraBasicDeployLab1Application.class, args);
 	}
 
 }
